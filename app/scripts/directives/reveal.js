aafc000c79d7063064338d769e1eb28fff390f43
Meunier Lionel <lmeunier@iocean.fr>
2015-02-08 18:41:00 +0100

    add first dev

--- app/scripts/directives/reveal.js
+++ app/scripts/directives/reveal.js
@@ -7,13 +7,36 @@
  * # reveal
  */
 angular.module('angularRevealApp')
-  .controller('RevealCtrl',['$scope',function($scope){
-    console.log('test');
+  .controller('RevealCtrl',['$scope','$window','$element',function($scope,$window,$element){
+    function calZoom(){
+      console.log('calcdu zoom');
+      var rW = $element.width() / $element.find('.slides').width();
+      var rH = $element.height() / $element.find('.slides').height();
+      var zoom;
+      if(rH > rW){
+        zoom = rW;
+      } else {
+        zoom = rH;
+      }
+      $element.find('.slides').css('zoom',zoom);
+    }
+
+    function testZoom() {
+      var slides = $element.find('.slides');
+      if(slides.width() > $element.width() || slides.height() > $element.height()){
+        calZoom();
+      }
+    }
+
+    angular.element($window).on('resize',function(){
+      testZoom();
+    });
+    testZoom();
   }])
   .directive('reveal', function () {
     return {
       restrict: 'EA',
-      //controller:'RevealCtrl',
+      controller:'RevealCtrl',
       transclude: true,
       replace: true,
       templateUrl:'template/reveal.html',
